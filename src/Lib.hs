b89912022aafcd8fe0633e082f6928e233ae7ea2
Constanza <constanza.luz.gonzalez@gmail.com>
2020-06-07 19:49:19 -0300

    agregar solucion punto 3 4 5 6

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,7 @@ import Text.Show.Functions
 laVerdad = True
 
 type Enfermedades = [String]
+type Hierba = Raton -> Raton
 
 data Raton = UnRaton {
     nombre :: String,
@@ -16,7 +17,7 @@ cerebro = UnRaton "Cerebro" 9 0.2 ["brucelosis", "sarampion", "tuberculosis"]
 bicenterrata = UnRaton "Bicenterrata" 256 0.2 []
 huesudo = UnRaton "Huesudo" 4 10 ["alta obesidad", "sinusitis"]
 
-hierbaBuena :: Raton -> Raton
+hierbaBuena :: Hierba
 hierbaBuena = modificarEdad rejuvenecerRaton
 
 rejuvenecerRaton :: Raton -> Float
@@ -25,7 +26,7 @@ rejuvenecerRaton raton = sqrt (edad raton)
 modificarEdad :: (Raton -> Float) -> Raton -> Raton
 modificarEdad f raton = raton { edad = f raton}
 
-hierbaVerde :: String -> Raton -> Raton
+hierbaVerde :: String -> Hierba
 hierbaVerde tipoHierba = modificarEnfermedades ( eliminarEnfermedadesTipoHierba tipoHierba)
 
 modificarEnfermedades :: (Raton -> Enfermedades) -> Raton -> Raton
@@ -41,7 +42,7 @@ obtenerUltimasLetras :: String -> String -> String
 obtenerUltimasLetras tipoHierba enfermedad = drop (length enfermedad - length tipoHierba) enfermedad
 
 
-alcachofa :: Raton -> Raton
+alcachofa :: Hierba
 alcachofa = modificarPeso reducirPesoPorcentaje
 
 modificarPeso :: (Raton -> Float) -> Raton -> Raton
@@ -52,7 +53,7 @@ reducirPesoPorcentaje raton
             | peso raton > 2 = peso raton - peso raton * 0.1
             | otherwise = peso raton - peso raton * 0.05
 
-hierbaZort :: Raton -> Raton
+hierbaZort :: Hierba
 hierbaZort = modificarEdad perderEdad . modificarEnfermedades perderEnfermedades
 
 perderEdad :: Raton -> Float
@@ -61,10 +62,60 @@ perderEdad raton = 0
 perderEnfermedades :: Raton -> Enfermedades
 perderEnfermedades raton = []
 
-hierbaDelDiablo :: Raton -> Raton
+hierbaDelDiablo :: Hierba
 hierbaDelDiablo = modificarPeso reducirPeso
 
 reducirPeso :: Raton -> Float
 reducirPeso raton 
         | (peso raton - 0.1 ) < 0 = 0
         | otherwise = peso raton - 0.1
+
+type Medicamento = [Hierba]
+
+pondsAntiAge :: Medicamento
+pondsAntiAge = [hierbaBuena, hierbaBuena, hierbaBuena, alcachofa]
+
+aplicarMedicamento :: Medicamento -> Raton-> Raton
+aplicarMedicamento medicamento raton  = foldl aplicar raton medicamento
+
+aplicar :: Raton -> Hierba -> Raton
+aplicar raton hierba = hierba raton
+
+reduceFatFast :: Int -> Medicamento
+reduceFatFast potencia = [hierbaVerde "obesidad"] ++ replicate potencia alcachofa
+
+sufijosInfecciosas = [ "sis", "itis", "emia", "cocos"]
+
+pdepCilina :: Medicamento
+pdepCilina = map hierbaVerde sufijosInfecciosas
+
+listaInfinita = iterate (+1) 1 
+
+lista = [1,2,3,4,5, 6]
+
+cantidadIdeal ::  (Int -> Bool) -> Int
+cantidadIdeal condicion =  encontrarIdeal condicion lista
+
+encontrarIdeal :: (Int -> Bool) -> [Int] -> Int
+encontrarIdeal condicion [] = 0
+encontrarIdeal condicion (cabeza:cola) 
+                        | condicion cabeza = cabeza
+                        |otherwise = encontrarIdeal condicion cola
+
+lograEstabilizar :: Medicamento -> [Raton] -> Bool
+lograEstabilizar medicamento  = all esEstable . map (aplicarMedicamento medicamento) 
+
+esEstable :: Raton -> Bool
+esEstable raton = peso raton > 1 && length(enfermedades raton) < 3
+
+--5)
+--a) No se puede, por que si todos estan estabilizados el all de una lista infinita no terminaria nunca
+--b) Si se puede, por que va a cortar cuando verifique que el raton no esta estable  
+
+--6)
+-- a) Hay que crear una funcion con el nombre de la nuerva hierba y utilizar las funciones existentes 
+-- como modificar edad , peso, etc. No habria que modificar ninguna funcion
+-- b) Generalizamos las funciones para no repetir codigo y utilizamos las funciones como valores
+-- c) Habria que modigicar reducirPesoPorcentaje, reducirPeso y esEstable
+
+
